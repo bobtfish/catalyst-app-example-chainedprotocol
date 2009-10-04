223ecdc90cc2cbd6411078eac19de5689db2a5ab
Tomas Doran <bobtfish@bobtfish.net>
2009-10-04 12:33:41 +0100

    Erm, that should work, but again Zoom isn't playing

--- lib/ChainedProtocol/Controller/Release.pm
+++ lib/ChainedProtocol/Controller/Release.pm
@@ -6,15 +6,31 @@ BEGIN { extends 'Catalyst::Controller' }
 
 sub root : Chained('/root') PathPart('') CaptureArgs(0) {}
 
-sub release : Chained('root') PathPart('release') CaptureArgs(0) {}
+sub release : Chained('root') PathPart('release') CaptureArgs(0) {
+    my ($self, $c) = @_;
+    my $rs = $c->stash->{rs} = $c->model('DB::Release')->search;
+}
 
 sub item : Chained('release') PathPart('') Args(1) {
     my ($self, $c, $id) = @_;
-    my $item = $c->stash->{item} = $c->model('DB::Release')->find($id);
+    my $item = $c->stash->{rs} = $c->stash->{rs}->find({ release_id => $id })->first;
     $c->detach('/error404') unless $item;
     $c->stash->{zoom} = { # FIXME - need deep selectors here..
         '.upc' => { -replace_content => $item->upc },
         '.title' => { -replace_content => $item->title },
+    };
+}
+
+sub list : Chained('release') PathPart('') Args(0) {
+    my ($self, $c) = @_;
+    my $rs = $c->stash->{rs};
+    $c->stash->{zoom} = {
+        '.list' => [
+            map { {
+                '.upc' => { -replace_content => $_->upc },
+                '.title' => { -replace_content => $_->title },
+            } } $rs->all
+        ],
     };
 }
 
